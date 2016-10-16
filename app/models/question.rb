a5415415fc08d87cf3e5ee574d0b549f7de9c1d0
kio <kioself1986@ya.ru>
2016-10-16 19:54:05 +0500

    validation_email_nick_text

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+
+  belongs_to :user
+  validates :text, :user, presence: true
+  validates :text, length: { maximum: 255 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,58 @@
+require 'openssl'
+
 class User < ActiveRecord::Base
+
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates :password, presence: true, on: :create #validates_presence_of
+  validates :password, confirmation: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, length: { maximum: 40 }, format: { with: /\A^@\w+\z/, on: :create}
+
+
+  before_save :encrypt_password
+  before_validation :username_downcase
+
+  def username_downcase
+    self.username.downcase!
+  end
+
+  def encrypt_password
+    if self.password.present?
+
+      # создаем т. н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хэш пароля — длинная уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сперва находим кандидата по email
+
+    # ОБРАТИТЕ внимание: сравнивается password_hash, а оригинальный пароль так никогда
+    # и не сохраняется нигде!
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
