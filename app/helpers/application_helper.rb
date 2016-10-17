19ba6a22ab929b87e9a6dfe14921592b7258f3e2
kio <kioself1986@ya.ru>
2016-10-17 20:31:02 +0500

    questions_count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def suffix(count, vopros, voprosa, voprosov)
+    if count%100 >= 5 && count%100 <= 20
+      voprosov
+
+    elsif count%10 == 1
+      vopros
+
+    elsif count%10 >= 2 && count%10 <= 4
+      voprosa
+
+    else
+      voprosov
+    end
+  end
 end
